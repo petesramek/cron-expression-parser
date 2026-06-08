8b326e241cd3fc2ca983f4f83cdf3def12408678
Pete Sramek <2333452+petesramek@users.noreply.github.com>
2026-06-08 16:33:06 +0200

    initial parse_field fn implementation

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,18 @@
 use crate::{Cron, CronError};
+use crate::field::Field;
 
 pub fn parse(_expression: &str) -> Result<Cron, CronError> {
     unimplemented!()
+}
+
+fn parse_field(input: &str) -> Result<Field, CronError> {
+    if input == "*" {
+        return Ok(Field::Any)
+    }
+
+    let value = input
+        .parse::<u32>()
+        .map_err(|_| CronError::InvalidExpression(format!("Invalid field value: {input}")))?;
+
+    return Ok(Field::Exact(value))
 }
